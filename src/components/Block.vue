fe20df404289ae03260eaa5bc838ef5f2c83017e
Saif Ur Rehman <itssaifurrehman@Saifs-MacBook-Air.local>
2021-11-11 14:15:01 +0500

    first commit

---
+++ src/components/Block.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="block" v-show="showBlock" @click="stopTimer">
+    click me
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['delay'],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true
+      this.startTimer()
+    }, this.delay)
+  }, /*,
+  updated() {
+      console.log("updated")
+  },
+unmounted(){
+    console.log("Unmounted")
+    //cool for Vue Rounting
+}*/
+  methods: {
+    startTimer() {
+      // start the timer, tick every 10ms
+      this.timer = setInterval(() => {
+        this.reactionTime += 10
+      }, 10)
+    },
+    stopTimer() {
+      // stop the timer
+      clearInterval(this.timer)
+      this.$emit('end', this.reactionTime)
+    }
+  }
+}
+</script>
+
+<style>
+  .block {
+    width: 400px;
+    border-radius: 20px;
+    background:  #0faf87;;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+  }
+</style>
